8a84ce9822ce98e746a97f649bd56887dc3b9c61
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-09-16 09:45:02 -0500

    Updated Pytest with Async Capability

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -1,14 +1,19 @@
+import pytest
+
 from fastapi.testclient import TestClient
 
 from ..main import app
 
 
+
 client = TestClient(app)
 
-
-def test_health_api():
+@pytest.mark.asyncio
+async def test_health_api():
     response = client.get("/health-check")
-    
+
     assert response.status_code == 200
-    assert response.json() == {"Status": "Healthy",
-        "Info" : "Welcome to Bearcat Resident Connect"}
+    assert response.json() == {
+        "Status": "Healthy",
+        "Info": "Welcome to Bearcat Resident Connect",
+    }
