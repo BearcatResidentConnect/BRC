335ad3344337f0f499de17cff433556380670a05
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-09-16 10:09:18 -0500

    Added ORM Model Skeleton

---
+++ api/models/model.py
@@ -0,0 +1,17 @@
+import enum
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Boolean,
+    Integer,
+    BigInteger,
+    Float,
+    String,
+    DateTime,
+    LargeBinary,
+    Enum,
+    func,
+)
+
+from sqlalchemy.orm import relationship
