390db48db0d744d2a9554caa8c04c396a0b78709
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-10-13 10:04:55 -0500

    Added New APIs to fetch data from the User Postings useful when integrating with frontend

--- api/app/views/user_postings.py
+++ api/app/views/user_postings.py
@@ -36,15 +36,15 @@ router = APIRouter(
 
 # GET API's
 @router.get(
-    "/user_postings/{user_id}",
-    response_model=UserPostingOut,
+    "/user-postings/{user_id}",
+    response_model=List[UserPostingOut],
     status_code=status.HTTP_200_OK,
 )
 async def get_user_postings_by_sis_id(
     user_id: int,
     session: Session = Depends(get_db_session),
     super_user_in: SuperUserIn = Depends(get_current_active_user),
-) -> UserPostingOut:
+) -> List[UserPostingOut]:
 
     """
     Get Matched Postings by user_id
@@ -52,9 +52,45 @@ async def get_user_postings_by_sis_id(
 
     return await _get_user_postings(session, user_id)
 
+@router.get(
+    "/user-posting/{user_id}/postings/{posting_id}",
+    response_model=UserPostingOut,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_posting_by_sis_id_and_posting_id(
+    user_id: int,
+    posting_id: int,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> UserPostingOut:
+
+    """
+    Get Matched Postings by user_id and Posting Id
+    """
+
+    return await _get_user_posting(session, user_id, posting_id)
+
 
 @router.get(
-    "/user_postings",
+    "/user-posting/postings/{posting_id}",
+    response_model=UserPostingOut,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_posting_by_posting_id(
+    posting_id: int,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> UserPostingOut:
+
+    """
+    Get Matched Posting by Posting Id ALone
+    """
+
+    return await _get_posting_by_id_alone(session, posting_id)
+
+
+@router.get(
+    "/user-postings",
     response_model=List[UserPostingOut],
     status_code=status.HTTP_200_OK,
 )
@@ -72,7 +108,7 @@ async def get_all_users_postings(
 
 # POST API's
 @router.post(
-    "/user_posting", response_model=UserPostingOut, status_code=status.HTTP_201_CREATED
+    "/user-posting", response_model=UserPostingOut, status_code=status.HTTP_201_CREATED
 )
 async def insert_user_posting(
     user_posting: UserPostingIn,
@@ -104,13 +140,12 @@ async def insert_user_posting(
 
 
 @router.post(
-    "/user_postings",
-    response_model = List[UserPostingOut],
+    "/user-postings",
+    response_model=List[UserPostingOut],
     status_code=status.HTTP_201_CREATED,
 )
 async def insert_user_postings(
     user_postings: List[UserPostingIn],
-    
     session: Session = Depends(get_db_session),
     super_user_in: SuperUserIn = Depends(get_current_active_user),
 ) -> List[UserPostingOut]:
@@ -138,42 +173,46 @@ async def insert_user_postings(
     return user_postings
 
 
-# # PUT Methods for Update operations
-# # @router.put("/user_posting/{posting_id}", response_model=UserPostingOut, status_code=status.HTTP_201_CREATED)
-# # async def update_user(
-# #     user_posting: UserPostingUpdate,
-# #     session: Session = Depends(get_db_session),
-# #     super_user_in: SuperUserIn = Depends(get_current_active_user)
-# #     # user: UserPostingUpdate = Body(embed=True), session: Session = Depends(get_db_session)
-# # ) -> UserPostingOut:
+# PUT Methods for Update operations
+@router.put(
+    "/user-posting/{posting_id}",
+    response_model=UserPostingOut,
+    status_code=status.HTTP_201_CREATED,
+)
+async def update_user(
+    user_posting: UserPostingUpdate,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user)
+    # user: UserPostingUpdate = Body(embed=True), session: Session = Depends(get_db_session)
+) -> UserPostingOut:
 
-# #     """
-# #     Update User data for given body parameters based on user_id \
+    """
+    Update User data for given body parameters based on user_id \
 
-# #         *** Only Include Modifiable Parameters ***
-# #     """
+        *** Only Include Modifiable Parameters ***
+    """
 
-# #     user_posting_dict = user_posting.dict()
+    user_posting_dict = user_posting.dict()
 
-# #     if "string" in user_posting_dict.values():
-# #         raise HTTPException(400, "Invalid Data Provided")
+    if "string" in user_posting_dict.values():
+        raise HTTPException(400, "Invalid Data Provided")
 
-# #     # Fetch User => Update
-# #     user = await _get_user_postings(session, user_posting_dict["user_id"])
+    # Fetch User => Update
+    user = await _get_user_postings(session, user_posting_dict["user_id"])
 
-# #     logger.debug("Fetched User ")
-# #     for k, v in user_posting_dict.items():
-# #         if k == "user_id":
-# #             continue
-# #         if v:
-# #             setattr(user, k, v)
+    logger.debug("Fetched User ")
+    for k, v in user_posting_dict.items():
+        if k == "user_id":
+            continue
+        if v:
+            setattr(user, k, v)
 
-# #     return user
+    return user
 
 
 # # Delete API
 # @router.delete(
-#     "/user_postings/{posting_id}",
+#     "/user-postings/{posting_id}",
 #     response_model=UserPostingOut,
 #     status_code=status.HTTP_200_OK,
 # )
@@ -196,6 +235,49 @@ async def insert_user_postings(
 
 
 # Helper Methods
+async def _get_posting_by_id_alone(session: Session, posting_id: int) -> UserPostingOut:
+
+    """
+    Query DB with given posting_id alone
+    """
+
+    _data = await session.execute(
+        select(UserPostingModel).where(UserPostingModel.posting_id == posting_id)
+    )
+
+    _data = _data.scalar()
+
+    if _data:
+        logger.debug("Fetched User Posting ")
+        return _data
+
+    raise HTTPException(404, "Posting Not Found")
+
+
+async def _get_user_posting(
+    session: Session, user_id: int, posting_id: int
+) -> UserPostingOut:
+
+    """
+    Query DB with given user_id and posting_id
+    """
+
+    _data = await session.execute(
+        select(UserPostingModel).where(
+            UserPostingModel.user_id == user_id,
+            UserPostingModel.posting_id == posting_id,
+        )
+    )
+
+    _data = _data.scalar()
+
+    if _data:
+        logger.debug("Fetched User Posting ")
+        return _data
+
+    raise HTTPException(404, NOT_FOUND_USER_POSTING)
+
+
 async def _get_user_postings(session: Session, user_id: int) -> UserPostingOut:
 
     """
@@ -206,13 +288,13 @@ async def _get_user_postings(session: Session, user_id: int) -> UserPostingOut:
         select(UserPostingModel).where(UserPostingModel.user_id == user_id)
     )
 
-    _data = _data.scalar()
+    _data = _data.scalars().all()
 
     if _data:
-        logger.debug("Fetched User ")
+        logger.debug("Fetched User Postings")
         return _data
 
-    raise HTTPException(404, NOT_FOUND_USER_POSTING)
+    raise HTTPException(404, NOT_FOUND_USER_POSTINGS)
 
 
 async def _get_all_user_postings(session: Session) -> List[UserPostingOut]:
@@ -228,7 +310,7 @@ async def _get_all_user_postings(session: Session) -> List[UserPostingOut]:
     _data = _data.scalars().all()
 
     if len(_data):
-        logger.debug("Fetched Users ")
+        logger.debug("Fetched All Users Postings")
         return _data
 
     raise HTTPException(404, NOT_FOUND_USER_POSTINGS)
