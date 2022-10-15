f515d067fc08524a642a26be5b9b92482a8d615f
Narendarkatta <k.narendarreddy111@gmail.com>
2022-10-14 22:14:30 -0500

    changes done in auth file

--- UI/Services/auth.service.ts
+++ UI/Services/auth.service.ts
@@ -2,35 +2,46 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://127.0.0.1:5000/api';
+const AUTH_API = 'http://127.0.0.1:5000/api/users/auth/token';
+const Api_Url = 'http://127.0.0.1:5000/api/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(user_name: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + '/users/auth', {
-      user_name,
-      password
-    }, httpOptions);
+    return this.http.post(
+      Api_Url + 'user',
+      {
+        user_name,
+        password,
+      },
+      httpOptions
+    );
   }
 
-  register(user_name: string,sid:string,first_name:string,last_name:string, email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + '/user', {
-      user_name,
-      sid,
-      first_name,
-      last_name,
-      email,
-      password
-    }, httpOptions);
+  register(sid:string, user_name: string,first_name:string,last_name:string, email: string, password: string): Observable<any> {
+    return this.http.post(
+      Api_Url + 'users',
+      {
+        sid,
+        user_name,
+        password,
+        first_name,
+        last_name,
+        email
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(Api_Url + 'signout', { }, httpOptions);
   }
 }
