d2341f8ecf2511a72117146906e9ccba15c22acb
Narendarkatta <k.narendarreddy111@gmail.com>
2022-10-14 22:25:31 -0500

    changes done in user service file

--- UI/Services/user.service.ts
+++ UI/Services/user.service.ts
@@ -2,22 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = 'http://127.0.0.1:5000/api/users';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPublicContent(): Observable<any> {
-    return this.http.get(API_URL + '/docs/users', { responseType: 'text' });
+    return this.http.get(API_URL + 'all', { responseType: 'text' });
   }
 
   getUserBoard(): Observable<any> {
     return this.http.get(API_URL + 'user', { responseType: 'text' });
   }
-
+  
   getModeratorBoard(): Observable<any> {
     return this.http.get(API_URL + 'mod', { responseType: 'text' });
   }
