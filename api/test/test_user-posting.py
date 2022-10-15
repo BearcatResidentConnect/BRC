dc96dbb650685cd36da676ea4caf8e768e4862a8
MANI <gaddameda96@gmail.com>
2022-10-14 20:33:14 -0500

    added userid and posting id testcases

---
+++ api/test/test_user-posting.py
@@ -0,0 +1,19 @@
+import pytest
+
+from httpx import AsyncClient
+
+from ..app.main import app
+
+
+@pytest.mark.asyncio
+async def test_userpostings():
+    data = {
+        "user_id":"userId",
+        "posting_id": "posting id" 
+    }
+    
+    async with AsyncClient(app=app, base_url="/user-posting/{user_id}/postings/{posting_id}") as ac:
+        response = await ac.post("/api/users/user-postings", json=data)
+
+    assert response.status_code == 200
+    assert response.json() == {"user_id":"userId","posting_id":"postingId"}
