40d6d4913420a5f48702facca49db424ae7f1f8b
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-11-16 09:09:48 -0600

    Updated Database Schema

--- api/app/models/models.py
+++ api/app/models/models.py
@@ -142,12 +142,13 @@ class RentalPosting(BaseDb):
 
     __tablename__ = "rental_postings"
 
-    posting_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    rental_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     #
-    # address_id = Column(BigInteger, ForeignKey("addresses.address_id"), nullable=False)
-    #
+    name = Column(String(255), nullable=False)
     phone = Column(BigInteger, nullable=False)
-    email = Column(BigInteger, nullable=False)
+    email = Column(String(50), nullable=True)
+    #
+    website = Column(String(255), nullable=False)
     #
     address1 = Column(String(255), nullable=False)
     address2 = Column(String(255), nullable=True)
@@ -159,9 +160,12 @@ class RentalPosting(BaseDb):
 
     def __init__(self, **kwargs):
 
+        # Contact
+        self.name = kwargs["name"]
+        self.website = kwargs["website"]
         self.phone = kwargs["phone"]
         self.email = kwargs["email"]
-        #
+        # Address
         self.address1 = kwargs["address1"]
         self.address2 = kwargs["address2"]
         self.address3 = kwargs["address3"]
@@ -169,4 +173,19 @@ class RentalPosting(BaseDb):
         self.state = kwargs["state"]
         self.country = kwargs["country"]
         self.zipcode = kwargs["zipcode"]
-        self.default = kwargs["default"]
+   
+     
+class UserApplication(BaseDb):
+
+    __tablename__ = "user_applications"
+
+    application_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    #
+    user_name = Column(String(255), ForeignKey("users.user_name"), nullable=False)  # FK
+    rental_id = Column(Integer, ForeignKey("rental_postings.rental_id"), nullable=False)  # FK
+    
+
+    def __init__(self, **kwargs):
+
+        self.user_name = kwargs["user_name"]
+        self.rental_id = kwargs["rental_id"]
