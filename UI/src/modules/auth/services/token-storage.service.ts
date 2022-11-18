3145bac3681f665462bfc0b711bf59abe3e7b759
Narendarkatta <k.narendarreddy111@gmail.com>
2022-11-17 21:47:34 -0600

    Interceptor  for sending token in the headers.

---
+++ UI/src/modules/auth/services/token-storage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'auth-token';
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+  constructor() { }
+
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+  public setToken(key:string, token: string): void {
+    window.sessionStorage.removeItem(key);
+    window.sessionStorage.setItem(key, token);
+  }
+  public getToken(key:string): string | null {
+    return window.sessionStorage.getItem(key);
+  }
+
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    return {};
+  }
+}
