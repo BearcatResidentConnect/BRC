83c76b1f5f6b7a69179fbdac5cf9cdd2ff4c23a7
MANI <gaddameda96@gmail.com>
2022-11-18 18:04:59 -0600

    side nav data
    side navigation data is added

---
+++ UI/src/modules/dashboard/data/side-nav.data.ts
@@ -0,0 +1,88 @@
+import { SideNavItems, SideNavSection } from '@modules/navigation/models';
+
+export const sideNavSections: SideNavSection[] = [
+    {
+        text: 'Main',
+        items: ['dashboard'],
+    },
+    {
+        text: 'Manage Listings',
+        items: ['tables', 'charts'],
+    },
+    {
+        text: 'Manage Accounts',
+        items: ['layouts', 'pages'],
+    },
+];
+
+export const sideNavItems: SideNavItems = {
+    dashboard: {
+        icon: 'tachometer-alt',
+        text: 'Dashboard',
+        link: '/dashboard',
+    },
+    charts: {
+        icon: 'user',
+        text: 'Profile',
+        link: '/charts',
+    },
+    tables: {
+        icon: 'user',
+        text: 'Add New',
+        link: '/tables',
+    },
+    layouts: {
+        icon: 'columns',
+        text: 'Layouts',
+        submenu: [
+            {
+                text: 'Static Navigation',
+                link: '/dashboard/static',
+            },
+            {
+                text: 'Light Sidenav',
+                link: '/dashboard/light',
+            },
+        ],
+    },
+    pages: {
+        icon: 'book-open',
+        text: 'Pages',
+        submenu: [
+            {
+                text: 'Authentication',
+                submenu: [
+                    {
+                        text: 'Login',
+                        link: '/auth/login',
+                    },
+                    {
+                        text: 'Register',
+                        link: '/auth/register',
+                    },
+                    {
+                        text: 'Forgot Password',
+                        link: '/auth/forgot-password',
+                    },
+                ],
+            },
+            {
+                text: 'Error',
+                submenu: [
+                    {
+                        text: '401 Page',
+                        link: '/error/401',
+                    },
+                    {
+                        text: '404 Page',
+                        link: '/error/404',
+                    },
+                    {
+                        text: '500 Page',
+                        link: '/error/500',
+                    },
+                ],
+            },
+        ],
+    },
+};
