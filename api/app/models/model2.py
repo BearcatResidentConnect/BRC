0ea16cfc0cd23e85c0c4251f88739ebcf55ca45f
Pasumarthi <S547089@nwmissouri.edu>
2022-11-18 21:07:56 -0600

    Added rental posting code

--- api/app/models/model2.py
+++ api/app/models/model2.py
@@ -29,6 +29,48 @@ class RentalPostingIn(RentalPostingBase):
     country: str
     zipcode: int
 
+    class RentalPosting(BaseDb):
+
+    __tablename__ = "rental_postings"
+
+    posting_id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    #
+    # address_id = Column(BigInteger, ForeignKey("addresses.address_id"), nullable=False)
+    #
+    phone = Column(BigInteger, nullable=False)
+    email = Column(BigInteger, nullable=False)
+    #
+    address1 = Column(String(255), nullable=False)
+    address2 = Column(String(255), nullable=True)
+    address3 = Column(String(255), nullable=True)
+    city = Column(String(50), nullable=False)
+    state = Column(String(50), nullable=False)
+    country = Column(String(50), nullable=False)
+    zipcode = Column(Integer, nullable=False)
+
+    def __init__(self, **kwargs):
+
+        self.phone = kwargs["phone"]
+        self.email = kwargs["email"]
+        #
+        self.address1 = kwargs["address1"]
+        self.address2 = kwargs["address2"]
+        self.address3 = kwargs["address3"]
+        self.city = kwargs["city"]
+        self.state = kwargs["state"]
+        self.country = kwargs["country"]
+        self.zipcode = kwargs["zipcode"]
+        self.default = kwargs["default"]
+
+
+
+        class RentalPostingOut(RentalPostingIn):
+    rental_id: int
+
+
+class RentalPostingUpdate(RentalPostingBase):
+    rental_id: int
+
 
 
 
