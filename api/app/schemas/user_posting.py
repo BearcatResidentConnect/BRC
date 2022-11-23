99c93be9168b0465074fc14813a550a7ae9050a2
Ramu Devarashetti <ramudevarashetti@gmail.com>
2022-11-23 15:39:54 -0600

    Added Property name attribute in Schemas

--- api/app/schemas/user_posting.py
+++ api/app/schemas/user_posting.py
@@ -17,6 +17,7 @@ class UserPostingBase(BaseModel):
 class UserPostingIn(UserPostingBase):
 
     accomedation_type: str = "Temporary"  # Temporary or Permanaent
+    name: str
     available_date: datetime = datetime.now()
     num_days: int = 7
     accomedated_date: Union[datetime, None] = None
@@ -25,7 +26,7 @@ class UserPostingIn(UserPostingBase):
     num_bedrooms: int = 1
     num_bathrooms: int = 1
     approx_rent: float = 200.0
-    #approx_distance: float = 1.0
+    # approx_distance: float = 1.0
     is_pet_friendly: bool = False
     parking_available: bool = False
     #
@@ -35,6 +36,7 @@ class UserPostingIn(UserPostingBase):
 class UserPostingOut(BaseModel):
 
     posting_id: int
+    name: str
     accomedation_type: str
     available_date: datetime
     num_days: int
@@ -44,7 +46,7 @@ class UserPostingOut(BaseModel):
     num_bedrooms: int
     num_bathrooms: int
     approx_rent: float
-    #approx_distance: float
+    # approx_distance: float
     is_pet_friendly: bool
     parking_available: bool
 
@@ -54,17 +56,19 @@ class UserPostingOut(BaseModel):
 
 # class UserPostingUpdate(UserPostingIn):
 #     posting_id = int
-    
+
+
 class UserPostingUpdate(BaseModel):
-    posting_id : int
-    
+    posting_id: int
+
     class Config:
         orm_mode = True
-        
+
+
 class UserPostingUpdate(BaseModel):
 
-    #user_name: Union[str, None] = None
-    posting_id : int # Required
+    # user_name: Union[str, None] = None
+    posting_id: int  # Required
     accomedation_type: Union[str, None] = None
     available_date: Union[datetime, None]
     num_days: Union[int, None] = None
@@ -73,8 +77,8 @@ class UserPostingUpdate(BaseModel):
     num_people_living: Union[int, None] = None
     num_bedrooms: Union[int, None] = None
     num_bathrooms: Union[int, None] = None
-    approx_rent:  Union[float, None] = None
-    #approx_distance: float = 1.0
+    approx_rent: Union[float, None] = None
+    # approx_distance: float = 1.0
     is_pet_friendly: Union[bool, None] = None
     parking_available: Union[bool, None] = None
     #
