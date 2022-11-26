a293422aa09f4dac163c9daed15946e00bc82cd5
Pasumarthi <S547089@nwmissouri.edu>
2022-11-25 20:25:16 -0600

    Added

--- api/app/views/rental posting.py
+++ api/app/views/rental posting.py
@@ -33,6 +33,59 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# GET API's
+@router.get(
+    "/user-postings/{user_id}",
+    response_model=List[UserPostingOut],
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_postings_by_sis_id(
+    user_id: int,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> List[UserPostingOut]:
+
+    """
+    Get Matched Postings by user_id
+    """
+    @router.get(
+    "/user-posting/{user_id}/postings/{posting_id}",
+    response_model=UserPostingOut,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_posting_by_sis_id_and_posting_id(
+    user_id: int,
+    posting_id: int,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> UserPostingOut:
+
+    """
+    Get Matched Postings by user_id and Posting Id
+    """
+
+    return await _get_user_posting(session, user_id, posting_id)
+
+
+@router.get(
+    "/user-posting/postings/{posting_id}",
+    response_model=UserPostingOut,
+    status_code=status.HTTP_200_OK,
+)
+async def get_user_posting_by_posting_id(
+    posting_id: int,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> UserPostingOut:
+
+    """
+    Get Matched Posting by Posting Id ALone
+    """
+
+    return await _get_posting_by_id_alone(session, posting_id)
+
+
+
 
 
 
