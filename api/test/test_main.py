affe2960841ed436343b8981891aac41166b115e
MANI <gaddameda96@gmail.com>
2023-01-27 19:52:44 -0600

    test main file modified
    solved the issue pytest is now working after upgrading of module

--- api/test/test_main.py
+++ api/test/test_main.py
@@ -3,20 +3,26 @@ import pytest
 from httpx import AsyncClient
 
 from ..app.main import app
+from fastapi.testclient import TestClient
 
 
-
-# client = TestClient(app)
+client = TestClient(app)
 
 @pytest.mark.asyncio
 async def test_health_api():
-    #response = client.get("/server-status")
+    # response = client.get("/server-status")
     
-    async with AsyncClient(app=app, base_url="http://server-status") as ac:
-        response = await ac.get("/server-status")
+    # async with AsyncClient(app=app, base_url="http://server-status") as ac:
+    #     response = await ac.get("/server-status")
 
+    # assert response.status_code == 200
+    # assert response.json() == {
+    #     "message": "Go Bearcats!! Welcome to Bearcat Resident Connect",
+    #     "Status": "Server Running 😊",
+    response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {
         "message": "Go Bearcats!! Welcome to Bearcat Resident Connect",
         "Status": "Server Running 😊",
     }
+    
