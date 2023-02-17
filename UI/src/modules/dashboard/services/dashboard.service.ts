df28149501f33de072be23370844d44ceead140b
Narendarkatta <k.narendarreddy111@gmail.com>
2023-02-17 10:45:19 -0600

    Updating dashboard service file
    Modifying dashboard files in order to achieve cross browser responsive

--- UI/src/modules/dashboard/services/dashboard.service.ts
+++ UI/src/modules/dashboard/services/dashboard.service.ts
@@ -1,11 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class DashboardService {
-    constructor() {}
+   
+    constructor(private http: HttpClient) { }
 
-    getDashboard$(): Observable<{}> {
-        return of({});
-    }
+    getDash( ): Observable<any> {
+        return this.http.get(environment.basePath + '/dashboard/analytics' )   
+      }
 }
