cb0e6c8e556c35561e91500967166ce55a132f94
shashank2514 <shashankpasumarthi25@gmail.com>
2023-02-17 17:47:42 -0600

    Added

--- api/app/models/model2.py
+++ api/app/models/model2.py
@@ -62,38 +62,6 @@ class RentalPostingIn(RentalPostingBase):
         self.zipcode = kwargs["zipcode"]
         self.default = kwargs["default"]
 
- class RentalPostingOut(RentalPostingIn):
-    rental_id: int
-
-
-class RentalPostingUpdate(RentalPostingBase):
-    rental_id: int
-
-    class Address(BaseDb):
-
-    __tablename__ = "addresses"
-
-    address_id = Column(BigInteger, primary_key=True, autoincrement=True, index=True)
-    #
-    #user_id = Column(BigInteger, ForeignKey("users.user_id"), nullable=False)
-    address1 = Column(String(255), nullable=False)
-    address2 = Column(String(255), nullable=True)
-    address3 = Column(String(255), nullable=True)
-    city = Column(String(100), nullable=False)
-    state = Column(String(100), nullable=False)
-    country = Column(String(2), nullable=False)
-    default = Column(Boolean, default=False)
-
-    def __init__(self, **kwargs):
-        #self.user_id = kwargs["user_id"]
-        self.address1 = kwargs["address1"]
-        self.address2 = kwargs["address2"]
-        self.address3 = kwargs["address3"]
-        self.city = kwargs["city"]
-        self.state = kwargs["state"]
-        self.country = kwargs["country"]
-        self.default = kwargs["default"]
-
 
 
 
