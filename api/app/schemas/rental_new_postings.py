3763a3bf6199fdeea541d8a3832bc397a82ab9b8
shashank2514 <shashankpasumarthi25@gmail.com>
2023-02-24 18:48:13 -0600

    Added

--- api/app/schemas/rental_new_postings.py
+++ api/app/schemas/rental_new_postings.py
@@ -28,6 +28,82 @@ async def get_user_by_sis_id(
     """
 
     return await _get_user(session, user_id)
+ @router.get("/users", response_model=List[UserOut], status_code=status.HTTP_200_OK)
+async def get_all_users(
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> List[UserOut]:
+
+    """
+    Get all Users
+    """
+
+    return await _get_all_users(session)
+
+
+# POST API's
+@router.post("/user", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+async def insert_user(
+    user: UserIn,
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> UserOut:
+
+    """
+    Create User if Not found
+    """
+
+    user_dict = user.dict()
+    if "string" in user_dict.values():
+        raise HTTPException(400, "Invalid Data Provided")
+
+    user_dict["password"] = get_password_hash(user_dict["password"])
+
+    try:
+
+        user = UserModel(**user_dict)
+        session.add(user)
+        await session.flush()
+        # await session.refresh(user)
+
+    except SQLAlchemyError as exc:
+
+        logger.error("Exception happend %s ", exc)
+        raise HTTPException(400, "Invalid Data Provided")
+
+    return user
+
+
+@router.post(
+    "/users", response_model=List[UserOut], status_code=status.HTTP_201_CREATED
+)
+async def insert_users(
+    users: List[UserIn],
+    session: Session = Depends(get_db_session),
+    super_user_in: SuperUserIn = Depends(get_current_active_user),
+) -> List[UserOut]:
+
+    """
+    Insert List of Users at a Time.
+    """
+
+    try:
+
+        users = [
+            UserModel(**user.dict())
+            for user in users
+            if "string" not in user.dict().values()
+        ]
+        # session.bulk_save_objects(users)
+        session.add_all(users)
+        await session.flush()
+
+    except SQLAlchemyError as exc:
+
+        logger.error("Exception happend %s ", exc)
+        raise HTTPException(400, "Invalid Data Provided")
+
+    return users
 
 
         
