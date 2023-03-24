676583c8045aed5796e96ed54c0508a44dd2dd33
MANI <gaddameda96@gmail.com>
2023-03-24 11:27:48 -0500

    Create test_user_login copy.py
    add login test file

---
+++ api/app/test/test_user_login copy.py
@@ -0,0 +1,66 @@
+import requests
+import pytest
+import asyncio
+
+BASE_URL = ""
+
+
+def test_user_post():
+    # data = {"name": "Item 1", "description": "Description for item 1"}
+    # response = requests.post(f"{BASE_URL}/items", json=data)
+    response = requests.post(f"{BASE_URL}/api/users")
+    assert response.status_code == 422
+
+def test_login_user():
+    data = {
+  "user_name": "mani",
+  "password": "mani@123"
+}
+    response = requests.post(f"{BASE_URL}/api/users/auth", json=data)
+
+    assert response.status_code == 200
+    assert response.json()["username"] == "mani"
+    assert isinstance(response.json(),dict)  
+
+def test_login_response_neg():
+    data = {
+  "user_name": "mani",
+  "password": "mani@123"
+}
+    response = requests.post(f"{BASE_URL}/api/users/auth", json=data)
+
+    assert response.status_code == 200
+    assert response.json()["username"] == "mani"
+    assert isinstance(response.json(),dict) 
+
+def test_user_response_put():
+    data = {
+  "user_name": "mani",
+  "password": "mani@123"
+}
+    response = requests.put(f"{BASE_URL}/api/user", json=data)
+
+    assert response.status_code == 201
+    assert response.status_code != 404
+    
+    assert isinstance(response.json(),dict)
+
+def test_login_response_neg_neg():
+    data = {
+  "user_name": "mani123",
+  "password": "mani@123"
+}
+    response = requests.post(f"{BASE_URL}/api/users/auth", json=data)
+
+    assert response.status_code == 404 
+
+def test_login_response_neg_neg():
+    data = {
+  "user_name": "mani13",
+  "password": "mani"
+}
+    response = requests.post(f"{BASE_URL}/api/users/auth", json=data)
+
+    assert response.status_code != 404 
+
+ 
