a88c99c9abe808ac179ebe78d65a6f1a8a55a8ce
shashank2514 <shashankpasumarthi25@gmail.com>
2023-03-24 20:19:56 -0500

    Added

--- api/app/schemas/user.py
+++ api/app/schemas/user.py
@@ -34,6 +34,46 @@ class UserIn(BaseModel):
         orm_mode = True
 
 
+        
+        class UserOut(BaseModel):
+    
+    user_name: str
+    first_name: str
+    last_name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+
+class UserUpdate(BaseModel):
+
+    user_id: int  # Can.t be changed
+    email: Union[str, None] = None
+    first_name: Union[str, None] = None
+    last_name: Union[str, None] = None
+    updated: Union[datetime, None] = None
+    active: bool = True
+    avatar: Union[bytes, None] = None
+
+    class Config:
+        orm_mode = True
+
+
+class SuperUserOut(BaseModel):
+    name: str
+    email: str
+    user_id: int
+    first_name: str
+    last_name: str
+    #department: str
+    user_name: str
+
+    class Config:
+        orm_mode = True
+
+
 
 
 
