223c0991e9125662a2e08e7a4270eeb0796fcf43
shashank2514 <shashankpasumarthi25@gmail.com>
2023-03-31 20:36:52 -0500

    Added

--- api/app/models/models.py
+++ api/app/models/models.py
@@ -58,67 +58,6 @@ class User(BaseDb):
 
 
 
-@router.post(
-    "/users", response_model=List[UserOut], status_code=status.HTTP_201_CREATED
-)
-async def insert_users(
-    users: List[UserIn],
-    session: Session = Depends(get_db_session),
-    super_user_in: SuperUserIn = Depends(get_current_active_user),
-) -> List[UserOut]:
-
-    """
-    Insert List of Users at a Time.
-    """
-
-    try:
-
-        users = [
-            UserModel(**user.dict())
-            for user in users
-            if "string" not in user.dict().values()
-        ]
-        # session.bulk_save_objects(users)
-        session.add_all(users)
-        await session.flush()
-
-    except SQLAlchemyError as exc:
-
-        logger.error("Exception happend %s ", exc)
-        raise HTTPException(400, "Invalid Data Provided")
-
-    return users
-
-@router.put("/user", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-async def update_user(
-    user: UserUpdate,
-    session: Session = Depends(get_db_session),
-    super_user_in: SuperUserIn = Depends(get_current_active_user)
-    # user: UserUpdate = Body(embed=True), session: Session = Depends(get_db_session)
-) -> UserOut:
-
-    """
-    Update User data for given body parameters based on user_id \
-
-        *** Only Include Modifiable Parameters ***
-    """
-
-    user_dict = user.dict()
-
-    if "string" in user_dict.values():
-        raise HTTPException(400, "Invalid Data Provided")
-
-    # Fetch User => Update
-    user = await _get_user(session, user_dict["user_id"])
-
-    logger.debug("Fetched User ")
-    for k, v in user_dict.items():
-        if k == "user_id":
-            continue
-        if v:
-            setattr(user, k, v)
-
-    return user
 
 
 
